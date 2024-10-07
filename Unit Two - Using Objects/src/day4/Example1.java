494cf3bad30b4c0e5d2fd8eb2b1366a74769941f
Caroline Ho <cho@bayviewglen.ca>
2024-10-07 10:49:01 -0400

    changes

---
+++ Unit Two - Using Objects/src/day4/Example1.java
@@ -0,0 +1,7 @@
+package day4;
+
+public class Example1 {
+    public static void main(String[] args) {
+        
+    }
+}
--- Unit Two - Using Objects/src/day4/Main.java
+++ Unit Two - Using Objects/src/day4/Main.java
@@ -2,27 +2,27 @@ package day4;
 
 public class Main {
     public static void main(String[] args) {
-        Teacher mrSmith = new Teacher("Mr. Smith", "Math", 10);
+    //     Teacher mrSmith = new Teacher("Mr. Smith", "Math", 10);
 
-        Student john = new Student("John", 85, 95.0);
-        Student jane = new Student("Jane", 90, 98.0);
+    //     Student john = new Student("John", 85, 95.0);
+    //     Student jane = new Student("Jane", 90, 98.0);
 
-        Course mathCourse = new Course("Algebra", mrSmith);
+    //     Course mathCourse = new Course("Algebra", mrSmith);
 
-        mathCourse.addStudent(john);
-        mathCourse.addStudent(jane);
+    //     mathCourse.addStudent(john);
+    //     mathCourse.addStudent(jane);
 
-        mathCourse.startClass();
+    //     mathCourse.startClass();
 
-        mrSmith.assignGrade(john, 85);
-        mrSmith.assignGrade(jane, 90);
+    //     mrSmith.assignGrade(john, 85);
+    //     mrSmith.assignGrade(jane, 90);
 
-        john.attendClass();
-        jane.attendClass();
+    //     john.attendClass();
+    //     jane.attendClass();
 
-        mathCourse.printCourseRoster();
+    //     mathCourse.printCourseRoster();
 
-        System.out.println("John will graduate: " + john.graduate());
-        System.out.println("Jane will graduate: " + jane.graduate());
+    //     System.out.println("John will graduate: " + john.graduate());
+    //     System.out.println("Jane will graduate: " + jane.graduate());
     }
 }
