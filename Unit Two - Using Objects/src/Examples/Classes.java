96061bed2cbb763e1876f0e76ebac8eec44b11b7
Caroline Ho <cho@bayviewglen.ca>
2024-10-20 20:08:22 -0400

    practice and work

---
+++ Unit Two - Using Objects/src/Examples/Classes.java
@@ -0,0 +1,8 @@
+package Examples;
+
+public class Classes {
+    Integer integer = new Integer(1);
+    int min = Integer.MIN_VALUE;
+    int max = Integer.MAX_VALUE;
+    System.out.println(min + " + " + max);
+}
---
+++ Unit Two - Using Objects/src/day6/StringExamples.java
@@ -0,0 +1,33 @@
+package day6;
+
+public class StringExamples {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "Hello";
+
+        System.out.println(s1 == s2);   // == checks the memory location for the variables to see if they are
+                                        // same --> does s1 and s2 store the same thing. YES memory location
+                                        // to a shared "Hello" 안녕하세요! 감사합니다! 
+
+        // only use == to compare primitives not object types
+
+        String str1 = new String("Hello");
+        String str2 = new String("Hello");
+
+        System.out.println(str1 == str2);           // false because new String() creates a new object for each
+
+        System.out.println(str1.equals(str2));      // true - same sequence of characters
+
+        System.out.println("Hello".equals(str2));   // true
+
+
+        System.out.println("Hello".compareTo("Hello"));         // 0
+        System.out.println("Dev".compareTo("Ian"));             // -5
+        System.out.println("Louisa".compareTo("Ian"));          // 3
+        System.out.println("Simon".compareTo("Stefano"));       // -11
+        System.out.println("Stefano".compareTo("Simon"));       // 11
+        System.out.println("Dog".compareTo("dog"));             // -32
+
+        System.out.println("abcdzz".compareTo("abcdz"));        // if number of letters in a word is greater than the other one then it counds the amount of letters
+    }
+}
