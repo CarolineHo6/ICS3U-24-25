c0fa1f3abaef2f83a606304db0d5d1981d23bbeb
Caroline Ho <cho@bayviewglen.ca>
2024-11-06 12:17:35 -0500

    homework

---
+++ UNIT THREE - BOOLEAN EXPRESSIONS AND IF STATEMENTS/src/day4/Work.java
@@ -0,0 +1,19 @@
+package day4;
+
+public class Work {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 2;
+        int c = 1;
+        int low;
+
+        if (a > b && b > c){
+            low = c;
+        } if (a > b && c > b){
+            low = b;
+        } else {
+            low = a;
+        }
+        System.out.println(a + b + c - low);
+    }
+}
