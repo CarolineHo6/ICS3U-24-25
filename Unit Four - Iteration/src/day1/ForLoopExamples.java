73af5929a751dc009a33b4383ba91ef6054755e0
Caroline Ho <cho@bayviewglen.ca>
2024-11-12 10:25:26 -0500

    new unit

---
+++ Unit Four - Iteration/src/day1/ForLoopExamples.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class ForLoopExamples {
+    public static void main(String[] args) {
+        // loops allow us to repeat code multiple times
+        // this unit we will learn the for loop and the while loop.
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
