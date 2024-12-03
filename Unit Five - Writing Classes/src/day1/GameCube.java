48d3bf4940365b2cff0263df1755ce72331f18d7
Caroline Ho <cho@bayviewglen.ca>
2024-12-03 17:47:15 -0500

    new Unit!

---
+++ Unit Five - Writing Classes/src/day1/GameCube.java
@@ -0,0 +1,33 @@
+package day1;
+
+public class GameCube {
+    // non=static instance variables
+    // also called attributes
+    // they belong to the instance of a class
+
+    private int numSides;
+    private int topSide;
+
+    //* Constructer sets the initial state of the instance */
+    public GameCube() {
+        numSides = 6;
+        roll();
+    }
+
+    // this refers to the instance variable because
+    // this refers to this object or this instance
+    // int numSides is just the argument/parameter
+    // this.numSides refers to the attribute numSides
+    public GameCube(int numSides) {
+        this.numSides = numSides;
+        roll();
+    }
+
+    public void roll() {
+        topSide = (int)(Math.random()*numSides) + 1;
+    }
+
+    public int getTopSide() {
+        return this.topSide;
+    }
+}
